dd4633fd145206e015b67d590b1f5548e1aa7fe3
pelectron <43130018+pelectron@users.noreply.github.com>
2024-01-13 20:23:29 +0100

    extended interface test

--- tests/interface.cpp
+++ tests/interface.cpp
@@ -1,19 +1,24 @@
 #include "poly.hpp"
 #include <catch2/catch_all.hpp>
 POLY_METHOD(method);
+struct method2{};
 POLY_PROPERTY(property);
-using OBJ = poly::Object<PROPERTIES(property(int)), METHODS(int(method)), 32>;
-using REF = poly::Interface<PROPERTIES(property(int)), METHODS(int(method))>;
+using OBJ = poly::Object<PROPERTIES(property(int)), METHODS(int(method), int(method2), int(method2, int)), 32>;
+using REF = poly::Interface<PROPERTIES(property(int)), METHODS(int(method), int(method2), int(method2, int))>;
 struct S1 {
   int property;
 };
 
 int extend(method, S1 &) { return 42; }
+int extend(method2, S1 &) { return 54; }
+int extend(method2, S1 &, int i) { return i+1; }
 
 struct S2 {
   int *p;
 };
 int extend(method, S2 &) { return 43; }
+int extend(method2, S2 &) { return 53; }
+int extend(method2, S2 &, int i) { return i+2; }
 
 int get(property, const S2 &) { return 5; }
 
@@ -29,6 +34,10 @@ TEMPLATE_TEST_CASE("generic interface test", "[interface]", REF, OBJ) {
     CHECK(interface.template call<method>() == 42);
     CHECK(interface.method() == 42);
   }
+  SECTION("S1::method2") {
+    CHECK(interface.template call<method2>() == 54);
+    CHECK(interface.template call<method2>(41) == 42);
+  }
   SECTION("S1::property") {
     CHECK(interface.property() == 79);
     interface.property(55);
@@ -43,6 +52,10 @@ TEMPLATE_TEST_CASE("generic interface test", "[interface]", REF, OBJ) {
   SECTION("S2::method") {
     CHECK(interface.template call<method>() == 43);
     CHECK(interface.method() == 43);
+  }
+  SECTION("S2::method2") {
+    CHECK(interface.template call<method2>() == 53);
+    CHECK(interface.template call<method2>(41) == 43);
   }
   SECTION("S2::property") {
     CHECK(i == 77);
